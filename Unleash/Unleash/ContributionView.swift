45c6fec315ff2614ff4f0d81a2c4ed3678f45305
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-03-17 10:04:58 +0200

    [200~modified:   Unleash/Unleash/ContributionView.swift ~

--- Unleash/Unleash/ContributionView.swift
+++ Unleash/Unleash/ContributionView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ContributionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("Contribute to Unleash")
+                    .font(.system(size: 20, weight: .bold))
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10).strokeBorder(Color.gray, style: StrokeStyle(lineWidth: 3, lineCap: .square, lineJoin: .miter, miterLimit: 10, dash: [10, 10], dashPhase: 1))
+                        .frame(height: 250)
+                    VStack {
+                        Image(systemName: "photo.on.rectangle.angled")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80)
+                            .foregroundColor(.gray)
+                            .overlay(
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .renderingMode(.original)
+                                    .frame(width: 25 ,height: 25)
+                                , alignment: .topTrailing
+                            )
+                            
+                            Text("Upload your photo to the largest")
+                            Text("library of open photography.")
+                    }
+                }
+                
+                
+                
+            }
+            .padding(10)
+            Spacer()
+        }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
+        .foregroundColor(.white)
     }
 }
 
